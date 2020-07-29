709cccb69e4db10af063b6e1bbda5ce13f77ca94
Ali Gottschall <aligottschall@protonmail.com>
2020-07-29 17:09:13 +0200

    Cleanup

--- src/main.py
+++ src/main.py
@@ -17,35 +17,12 @@ playground = pygame.image.load(get_path("assets/background.jpg")).convert()
 
 stones = pygame.sprite.Group()
 
-
-<<<<<<< HEAD
 # states: idle, selected
 IDLE = "IDLE"
 SELECTED = "SELECTED"
 WAITING = "WAITING"
-=======
+
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-
-class Block(pygame.sprite.Sprite):
-
-    # Constructor. Pass in the color of the block,
-    # and its x and y position
-    def __init__(self, color=ORANGE, width=RADIUS, height=RADIUS):
-        # Call the parent class (Sprite) constructor
-        pygame.sprite.Sprite.__init__(self)
-
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.image.load(str(Path("src/assets/bee.png").absolute()))
-        self.image = pygame.transform.scale(self.image, (40, 40))
-
-        # Fetch the rectangle object that has the dimensions of the image
-        # Update the position of this object by setting the values of rect.x and rect.y
-        self.rect = self.image.get_rect()
-
-
-playground = pygame.image.load(str(Path("src/assets/background.jpg").absolute())).convert()
->>>>>>> 7732189011473c798b4e5351566f33d4383c621c
 
 state = IDLE
 move_number = 0
