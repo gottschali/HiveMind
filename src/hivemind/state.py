befbc4dc25dc5d67f96ce8d46d9dbd7053075af0
Aliuakbar <ali@ptwenty>
2021-01-21 11:05:51 +0100

    Children return tuple

--- src/hivemind/state.py
+++ src/hivemind/state.py
@@ -159,15 +159,15 @@ class State:
                     opts.append(Move(origin, destination))
         return tuple(opts) if opts else (Pass(),)
 
-    def possible_actions_for_hex(self, hex):
+    def possible_actions_for_hex(self, hex: Hex) -> Iterator[Action]:
         for action in self.possible_actions:
+            # Currently only moves supporeted
             if isinstance(action, Move):
                 if action.origin == hex:
                     yield action.destination
 
-    def children(self):
-        for action in self.possible_actions:
-            yield self + action
+    def children(self) -> Tuple[Action]:
+        return tuple(self + action for action in self.possible_actions)
 
     def next_state(self, policy=random.choice):
         return self + policy(self.possible_actions)
