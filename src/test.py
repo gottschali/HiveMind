f3ae07dd2ce6b6f9510aec5b611271e94e9d5e35
Aliuakbar <ali@ptwenty>
2021-01-22 11:09:46 +0100

    Performance test

--- src/test.py
+++ src/test.py
@@ -1,19 +1,31 @@
+import time
 
 from hivemind.state import *
 from hivemind.hive import *
 from hivemind.hex import *
 
-root = Root()
-print(root._unique_availables())
-print(root.possible_actions)
-state = root
-i = 0
-while True:
-    try:
-        state = state.next_state()
-        i += 1
-    except Exception as e:
-        print(state)
-        print(state.possible_actions)
-        print("i: ", i)
-        raise e
+def fuzz(n):
+    state = Root()
+    for i in range(n):
+        try:
+            state = state.next_state()
+            i += 1
+        except Exception as e:
+            print(state)
+            print(state.possible_actions)
+            print("i: ", i)
+            raise e
+
+n = 1000
+k = 3
+total = 0
+for i in range(k):
+    start = time.perf_counter()
+    fuzz(n)
+    elapsed = time.perf_counter() - start
+    total += elapsed
+    print(f"Took {elapsed:.3f} seconds")
+print("#" * 50)
+avg = total / k
+print(f"Average time {avg:.3f} seconds")
+print(f"Single iteration avg {avg / n * 1e3 :.3f} milli seconds")
