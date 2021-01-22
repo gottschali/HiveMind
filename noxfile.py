020a8e288b4e6bb633aa43d080e8d38462a8f5d8
Aliuakbar <ali@ptwenty>
2021-01-22 15:28:38 +0100

    Remove install_with_contraints

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,7 @@
 import nox
-import tempfile
 
 nox.options.sessions = "lint", "mypy", "tests"
 locations = "src/hivemind", "tests"
-
-def install_with_constraints(session, *args, **kwargs):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 @nox.session
 def tests(session):
@@ -31,15 +18,14 @@ def black(session):
 @nox.session
 def lint(session):
     args = session.posargs or locations
-    install_with_constraints(session,
-                             "flake8",
-                             "flake8-annotations",
-                             "flake8-black",
-                             "flake8-import-order")
+    session.install("flake8",
+                    "flake8-annotations",
+                    "flake8-black",
+                    "flake8-import-order")
     session.run("flake8", *args)
 
 @nox.session
 def mypy(session):
     args = session.posargs or locations
-    install_with_constraints(session, "mypy")
+    session.install("mypy")
     session.run("mypy", *args)
