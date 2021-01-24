432d33d564908a824f60c3493f321d69cc3ad6c2
Aliukabar <aligottschall22@gmail.com>
2021-01-24 17:58:40 +0100

    Formatting

--- hivehost/app.py
+++ hivehost/app.py
@@ -1,7 +1,7 @@
-import logging
 import json
+import logging
 
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit
 
 from hivemind.state import *
@@ -9,11 +9,14 @@ from hivemind.state import *
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="app.log", filemode="w", format="%(name)s - %(levelname)s - %(message)s"
+)
 
 # TODO: standardize JSON de-/serialization
 # TODO: send moveable pieces (one-hive) with state
 # In future threading/async
+
 
 @app.route("/")
 def index():
@@ -24,19 +27,24 @@ def index():
 def play_self():
     return render_template("play.html")
 
+
 @app.route("/play/ai")
 def play_ai():
     return render_template("play.html")
+
 
 @app.route("/play/online")
 def play_online():
     return render_template("play.html")
 
+
 games = {}
+
 
 def emit_state(sid):
     json_state = games[sid].to_json()
     emit("sendstate", json_state)
+
 
 @socketio.on("connect")
 def connect_handler():
@@ -44,16 +52,19 @@ def connect_handler():
     print(f"{request.sid} connected")
     reset_handler()
 
+
 @socketio.on("disconnect")
 def disconnect_handler():
     # Delete the game
     del games[request.sid]
     print(f"{request.sid} disconnected")
 
+
 @socketio.on("ai_action")
 def ai_action_handler():
     games[request.sid] = games[request.sid].next_state()
     emit_state(request.sid)
+
 
 @socketio.on("auto_action")
 def auto_action_handler():
@@ -61,6 +72,7 @@ def auto_action_handler():
         games[request.sid] = games[request.sid].next_state()
         emit_state(request.sid)
         socketio.sleep(0.02)
+
 
 @socketio.on("reset")
 def reset_handler():
@@ -102,7 +114,9 @@ def options_handler(data):
             if isinstance(action, Move):
                 if action.origin == origin:
                     opts.append(action.destination)
-        return json.dumps([{"q": h.q, "r": h.r, "h": state.hive.height(h)} for h in opts])
+        return json.dumps(
+            [{"q": h.q, "r": h.r, "h": state.hive.height(h)} for h in opts]
+        )
 
     if action_type == "drop":
         insect = Insect(int(first))
