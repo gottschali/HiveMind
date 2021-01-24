e44c64b2694a285b511664ab1f30b3c944e4373d
Aliukabar <aligottschall22@gmail.com>
2021-01-24 20:43:09 +0100

    Remove logging

--- monte.py
+++ monte.py
@@ -4,21 +4,8 @@ from hivemind.state import State
 from mcts.node import MonteCarloTreeSearchNode
 from mcts.search import MonteCarloTreeSearch
 
-LEVEL = logging.WARN
-
-logging.basicConfig(
-    filename="test.log", filemode="w", format="%(filename)s: %(message)s", level=LEVEL
-)
-
-logger = logging.getLogger()
-console_handler = logging.StreamHandler()
-console_handler.setLevel(LEVEL)
-formatter = logging.Formatter("%(levelname)s %(filename)s:  %(message)s")
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
 root = State()
 node = MonteCarloTreeSearchNode(root)
 search = MonteCarloTreeSearch(node)
-r = search.best_action(10)
-print(r)
+r = search.best_action(100)
+print(r.state)
