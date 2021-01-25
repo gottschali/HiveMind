e8dc2dc1e0d75dd868a80ce9325e98b4cdd0b397
Aliuakbar <ali@ptwenty>
2021-01-25 16:40:03 +0100

    MVC

--- hivehost/static/index.js
+++ hivehost/static/index.js
@@ -8,27 +8,17 @@ const canvas = document.querySelector('#c');
 var Paint = new Painter(canvas);
 
 
-// Control for moving around the scene
-
-
 // Connect to the Socket.IO server.
 // The connection URL has the following format, relative to the current page:
 //     http[s]://<domain>:<port>[/<namespace>]
 var socket = io();
 $(document).ready(function() {
-    // Event handler for new connections.
-    // The callback function is invoked when a connection with the
-    // server is established.
     socket.on('connect', function() {
         console.log("Client connected");
     }, function(json) { // Callback
         Paint.drawState(json);
     });
 
-    // Event handler for server sent data.
-    // The callback function is invoked whenever the server emits data
-    // to the client. The data is then displayed in the "Received"
-    // section of the page.
     socket.on('sendstate', function(json) {
         console.log("Received State");
         Paint.drawState(json);
@@ -36,9 +26,6 @@ $(document).ready(function() {
         return false;
     });
 
-    // Handlers for the different forms in the page.
-    // These accept data from the user and send it to the server in a
-    // variety of ways
     $('form#test').submit(function(event) {
         console.log("Client requests Action");
         socket.emit('ai_action');
@@ -84,80 +71,110 @@ const WAITING = "waiting";
 const SELECTED = "selected";
 var state = IDLE;
 
-
-function onDocumentMouseDown( event ) {
-    console.log("state: ", state);
-    if (state === WAITING) return;
-    event.preventDefault();
-    var mouse3D = new Vector3(
-        ( ( event.clientX - canvas.offsetLeft ) / canvas.width ) * 2 - 1,
-        -( ( event.clientY - canvas.offsetTop ) / canvas.height ) * 2 + 1,
-          0.5 );
-    var raycaster =  new Raycaster();
-    raycaster.setFromCamera( mouse3D, Paint.camera );
-
-    var intersects = raycaster.intersectObjects( Paint.tileArray );
-    var intersectsTarget = raycaster.intersectObjects( Paint.highlightArray );
-    var intersectsDrop = raycaster.intersectObjects( Paint.dropArr );
-    console.log(intersectsTarget);
-    if ( intersectsTarget.length > 0 ) {
-
-        var target = intersectsTarget[ 0 ];
-        const newHex = Paint.layout.pixelToHex(target.point).round();
-        console.log(newHex);
-        state = IDLE;
-        // TODO: abstract state change
-        Paint.highlightGroup.clear();
-        console.log("actiontype", actionType);
-        emitAction(newHex);
+class Game {
+    constructor(player0, player1) {
+        this.player0 = player0;
+        this.player1 = player1;
+        this.current_player = 0;
     }
-    else if (intersectsDrop.length > 0) {
-        var drop = intersectsDrop[ 0 ].object;
-        console.log(drop);
-        dropSelection = drop.insect;
-        previousSelection = null;
-        state = SELECTED;
-        console.log("selected drop", dropSelection);
-        actionType = "drop";
-        firstArg = dropSelection;
-        emitOptions();
+}
+class Player {
+
+}
+Player.prototype = {
+    action: function(args) {
+        
     }
-    else if ( intersects.length > 0 ) {
-        dropSelection = null;
-        var selected = intersects[ 0 ];
-        console.log("selected-highligh", selected, previousSelection);
-        if (previousSelection !== null && previousSelection.id == selected.object.id) {
-            console.log("selected the same again", previousSelection.previous);
-            selected.object.material.color.setHex( previousSelection.previous );
-            previousSelection = null;
-            state = IDLE;
-        }
-        else if (previousSelection === null || previousSelection.id != selected.object.id) {
-            if (previousSelection !== null) {
-                previousSelection.material.color.setHex( previousSelection.previous );
-            }
-            previousSelection = selected.object;
-            previousSelection.previous = selected.object.material.color.getHex();
-            selected.object.material.color.set( CONSTANTS.GREEN );
-            const newHex = Paint.layout.pixelToHex(selected.point).round();
+}
+class LocalPlayer extends Player {
+    constructor() {
+        super();
+        canvas.addEventListener( "click", this.onDocumentMouseDown );
+    }
+    onDocumentMouseDown( event ) {
+        console.log("state: ", state);
+        if (state === WAITING) return;
+        event.preventDefault();
+        var mouse3D = new Vector3(
+            ( ( event.clientX - canvas.offsetLeft ) / canvas.width ) * 2 - 1,
+            -( ( event.clientY - canvas.offsetTop ) / canvas.height ) * 2 + 1,
+            0.5 );
+        var raycaster =  new Raycaster();
+        raycaster.setFromCamera( mouse3D, Paint.camera );
+
+        var intersects = raycaster.intersectObjects( Paint.tileArray );
+        var intersectsTarget = raycaster.intersectObjects( Paint.highlightArray );
+        var intersectsDrop = raycaster.intersectObjects( Paint.dropArr );
+        console.log(intersectsTarget);
+        if ( intersectsTarget.length > 0 ) {
+
+            var target = intersectsTarget[ 0 ];
+            const newHex = Paint.layout.pixelToHex(target.point).round();
             console.log(newHex);
+            state = IDLE;
+            // TODO: abstract state change
+            Paint.highlightGroup.clear();
+            console.log("actiontype", actionType);
+            emitAction(newHex);
+        }
+        else if (intersectsDrop.length > 0) {
+            var drop = intersectsDrop[ 0 ].object;
+            console.log(drop);
+            dropSelection = drop.insect;
+            previousSelection = null;
             state = SELECTED;
-            actionType = "move";
-            firstArg = newHex;
-
+            console.log("selected drop", dropSelection);
+            actionType = "drop";
+            firstArg = dropSelection;
             emitOptions();
+        }
+        else if ( intersects.length > 0 ) {
+            dropSelection = null;
+            var selected = intersects[ 0 ];
+            console.log("selected-highligh", selected, previousSelection);
+            if (previousSelection !== null && previousSelection.id == selected.object.id) {
+                console.log("selected the same again", previousSelection.previous);
+                selected.object.material.color.setHex( previousSelection.previous );
+                previousSelection = null;
+                state = IDLE;
+            }
+            else if (previousSelection === null || previousSelection.id != selected.object.id) {
+                if (previousSelection !== null) {
+                    previousSelection.material.color.setHex( previousSelection.previous );
+                }
+                previousSelection = selected.object;
+                previousSelection.previous = selected.object.material.color.getHex();
+                selected.object.material.color.set( CONSTANTS.GREEN );
+                const newHex = Paint.layout.pixelToHex(selected.point).round();
+                console.log(newHex);
+                state = SELECTED;
+                actionType = "move";
+                firstArg = newHex;
 
+                emitOptions();
+
+            }
         }
     }
 }
-canvas.addEventListener( "click", onDocumentMouseDown );
+class AIPlayer extends Player {
+    constructor() {
+        super();
+    }
+
+}
+
+const localPlayer = new LocalPlayer();
+const aiPlayer = new AIPlayer();
+const game = new Game(localPlayer, localPlayer);
+
 
 // call this only in static scenes (i.e., if there is no animation loop)
 Paint.controls.addEventListener( 'change', Paint.render.bind(Paint) ); 
 
-window.addEventListener('resize', Paint.render);
-window.addEventListener('keydown', (e) => {
-    e.preventDefault();
-});
+window.addEventListener('resize', Paint.render.bind(Paint));
+// window.addEventListener('keydown', (e) => {
+    // e.preventDefault();
+// });
 
 Paint.render(); // Init Render
