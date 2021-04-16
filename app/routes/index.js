285e36d360803289660f79fc0662875c2c4be548
Aliukabar <aligottschall22@gmail.com>
2021-04-16 17:50:05 +0200

    Pug tweaks

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'HiveMind' });
+
+router.get('/', function(req, res, next) {
+    res.redirect('/index')
+});
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'HiveMind' });
 });
 
 
