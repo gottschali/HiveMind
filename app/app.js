a680da2d1280624f91fd4a6cf350075fdf4e15e5
Gottschali <aligottschall22@gmail.com>
2021-04-28 09:23:02 +0200

    Serve static files from /static and not root and use nodemodules

--- app/app.js
+++ app/app.js
@@ -17,8 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/static', express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../dist')));
+app.use('/static', express.static(path.join(__dirname, '../node_modules', 'jquery', 'dist')));
+app.use('/static', express.static(path.join(__dirname, '../node_modules', 'bootstrap', 'dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- src/client/view/models.js
+++ src/client/view/models.js
@@ -3,21 +3,21 @@ import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 let models = {};
 const gltfLoader = new GLTFLoader();
 
-gltfLoader.load('/objects/bee/bumblebee.gltf', (gltf) => {
+gltfLoader.load('/static/objects/bee/bumblebee.gltf', (gltf) => {
      models["BEE"] = gltf.scene
 });
-gltfLoader.load('/objects/spider/spider.gltf', (gltf) => {
+gltfLoader.load('/static/objects/spider/spider.gltf', (gltf) => {
      models["SPIDER"] = gltf.scene
 });
 
-gltfLoader.load('/objects/grasshopper/grasshopper.gltf', (gltf) => {
+gltfLoader.load('/static/objects/grasshopper/grasshopper.gltf', (gltf) => {
      models["GRASSHOPPER"] = gltf.scene
 });
-gltfLoader.load('/objects/ant/ant.gltf', (gltf) => {
+gltfLoader.load('/static/objects/ant/ant.gltf', (gltf) => {
      models["ANT"] = gltf.scene
 });
 
-gltfLoader.load('/objects/beetle/beetle.gltf', (gltf) => {
+gltfLoader.load('/static/objects/beetle/beetle.gltf', (gltf) => {
     models["BEETLE"] = gltf.scene
 });
 
