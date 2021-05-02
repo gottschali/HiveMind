c41132e63ef7ba4d1a280116023d965334cebbec
Gottschali <aligottschall22@gmail.com>
2021-05-02 11:56:51 +0200

    Remov user router

--- app/app.js
+++ app/app.js
@@ -8,7 +8,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -28,7 +27,6 @@ app.use('/static', express.static(path.join(__dirname, '../node_modules', 'boots
 app.use(favicon(path.join(__dirname, '..', 'public', 'favicon', 'favicon.ico')))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
