157a53aa902c59ebe569c8c23d11f19fbc0b64cd
Gottschali <aligottschall22@gmail.com>
2021-08-08 17:41:16 +0200

    Ts Stone

--- cra-hive/src/shared/model/stone.ts
+++ cra-hive/src/shared/model/stone.ts
@@ -1,4 +1,13 @@
-export function Stone(insect, team) {
+import Insect from './insects';
+import Team from './teams';
+
+export default class Stone {
+  insect: Insect;
+  team: Team;
+
+  constructor(insect: Insect, team: Team) {
     this.insect = insect;
     this.team = team;
+  }
 }
+
