f0ac6e3dd64b78a2905906e77e67193a3d048ca0
Ali Gottschall <aligottschall@protonmail.com>
2021-08-13 15:40:26 +0200

    OnlineGame

--- cra-hive/src/client/components/OnlineGame.tsx
+++ cra-hive/src/client/components/OnlineGame.tsx
@@ -1,6 +1,21 @@
 import { useParams } from 'react-router-dom'
+import { useInteractiveController } from '../controllers/useInteractiveController';
+import RemoteGame from '../game/RemoteGame';
+
 
 export default function OnlineGame() {
     const { gid } = useParams();
-    return <h1> Game ID: {gid} </h1>
-}
+    const p1 = useInteractiveController;
+    const p2 = (submitAction, state) => {
+        return ({
+            highlighted: [],
+            handleBoardClick: () => console.log("Remote player's turn"),
+            handleDropClick: () => console.log("Remote player's turn")
+        });
+    };
+    return (
+        <div>
+            <h1> Game ID: {gid} </h1>
+            <RemoteGame p1={p1} p2={p2} />
+        </div>
+    )}
