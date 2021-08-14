330d89fd080a77b9b80e30cdc16727c2e7974e5e
Ali Gottschall <aligottschall@protonmail.com>
2021-08-14 15:54:35 +0200

    Websockets works with forceupdate

--- cra-hive/src/client/components/OnlineGame.tsx
+++ cra-hive/src/client/components/OnlineGame.tsx
@@ -2,20 +2,49 @@ import { useParams } from 'react-router-dom'
 import { useInteractiveController } from '../controllers/useInteractiveController';
 import RemoteGame from '../game/RemoteGame';
 
+import { useEffect, useState } from 'react';
+import socketIOClient from "socket.io-client";
+
+import useHiveState from "../game/useHiveState";
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
 
 export default function OnlineGame() {
     const { gid } = useParams();
     const p1 = useInteractiveController;
-    const p2 = (submitAction, state) => {
-        return ({
-            highlighted: [],
-            handleBoardClick: () => console.log("Remote player's turn"),
-            handleDropClick: () => console.log("Remote player's turn")
-        });
-    };
+    const p2 = useInteractiveController;
+    const [socket, setSocket] = useState(null)
+    // const p2 = (submitAction, state) => {
+        // return ({
+            // highlighted: [],
+            // handleBoardClick: () => console.log("Remote player's turn"),
+            // handleDropClick: () => console.log("Remote player's turn")
+        // });
+    // };
+    const forceUpdate = useForceUpdate();
+
+    const [state, apply] = useHiveState();
+
+    useEffect( () => {
+        const socket = socketIOClient();
+        socket.emit('joinGame', gid)
+        socket.on('updateAction', (action) => {
+            apply(action);
+            forceUpdate();
+            console.log("updating ", action)
+        })
+        setSocket(socket)
+        socket.onAny( (...args)=> {
+            console.log(...args)
+        })
+
+    }, [])
     return (
         <div>
             <h1> Game ID: {gid} </h1>
-            <RemoteGame p1={p1} p2={p2} />
+            {socket ? <RemoteGame p1={p1} p2={p2} state={state} apply={apply} socket={socket} /> : 'Connectin...'}
         </div>
     )}
--- cra-hive/src/client/game/RemoteGame.tsx
+++ cra-hive/src/client/game/RemoteGame.tsx
@@ -1,18 +1,7 @@
 import InteractiveGame from "../game/InteractiveGame";
-import useHiveState from "./useHiveState";
 
-export default function RemoteGame({ p1, p2 }) {
-    const [state, apply] = useHiveState();
-
-    const submitAction = (action) => {
-            console.log(`Remote Submitting ${action}`)
-            if (state.isLegal(action)) {
-                apply(action);
-                return true;
-            }
-            return false;
-    };
-
+export default function RemoteGame({ p1, p2, state, apply, socket }) {
+    const submitAction = (action) => socket.emit("intendAction", {action: action})
     const player = ((state.turnNumber % 2 == 0) ? p1 : p2)(submitAction, state)
     return <InteractiveGame 
         state={state}
