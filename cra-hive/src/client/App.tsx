bc540946525b29523de1a1c8bdc4842c7e4a92e8
Ali Gottschall <aligottschall@protonmail.com>
2021-08-15 16:36:28 +0200

    Add semanticui

--- cra-hive/src/client/App.tsx
+++ cra-hive/src/client/App.tsx
@@ -8,6 +8,7 @@ import {
   Route,
 } from "react-router-dom";
 import GameList from './components/GameList';
+import 'semantic-ui-css/semantic.min.css'
 
 export default function App() {
   return (
