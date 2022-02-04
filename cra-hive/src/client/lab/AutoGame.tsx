04cd0f4708ef9ee7cd31e188c205d8841625f7f8
Gottschali <aligottschall@protonmail.com>
2022-02-04 18:32:30 +0100

    AutoGame experimentations

--- cra-hive/src/client/lab/AutoGame.tsx
+++ cra-hive/src/client/lab/AutoGame.tsx
@@ -1,15 +1,42 @@
-import Hive from '../canvas/Hive'
 import useHiveGame from "../game/useHiveGame";
 import React, { useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber'
+import {layoutFlat} from '../../shared/hexlib'
+import Stone from '../canvas/Stone'
+
+const Hive = ({ hive, handleClick=()=>console.log("No click handler given"), highlighted=[], team=null, interactive=true }) => {
+    const [layout,] = useState(layoutFlat)
+    let stones = [];
+    for (const hex of hive.map.keys()) {
+        hive.map.get(hex).forEach((stone, height) => {
+            stones.push(
+                <Stone
+                    hex={hex}
+                    stone={stone}
+                    height={height}
+                    layout={layout}
+                    handleClick={handleClick} />
+            )
+        });
+    }
+    return (
+        <div style={{ position: "relative", height: '50vw' }}>
+            <Canvas camera={{ near: 0.1, far: 100 }} >
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                {stones}
+            </Canvas>
+        </div>
+    )
+}
 
 export default function AutoGame() {
     const {state, apply} = useHiveGame();
 
   const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(true);
+  const [isActive, ] = useState(true);
 
   const submitAction = (action) => {
-            console.log(`Submitting ${action}`)
             if (state.isLegal(action)) {
                 apply(action);
                 return true;
@@ -29,17 +56,10 @@ export default function AutoGame() {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds, state, submitAction]);
+  });
 
-    return <div>
-      <div className="time">
-        {seconds}s
-
-        <button onClick={() => setIsActive(!isActive)}>{(isActive ? "Pause" : "Continue")}</button>
-      </div>
-      <Hive
-        hive={state.hive}
-        interactive={false}
-        team={state.team} />
-    </div>
+    return <Hive
+             hive={state.hive}
+             interactive={false}
+             team={state.team} />
 }
