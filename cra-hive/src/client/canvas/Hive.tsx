fe17c51e1bd816ac77f5ec4e7013a6254d90b231
Gottschali <aligottschall@protonmail.com>
2022-02-15 15:42:40 +0100

    More options for Hive Component

--- cra-hive/src/client/canvas/Hive.tsx
+++ cra-hive/src/client/canvas/Hive.tsx
@@ -7,9 +7,16 @@ import * as HEX from '../../shared/hexlib'
 import Stone from './Stone'
 import HighlightStone from './HighlightStone'
 
-import { Button } from 'semantic-ui-react'
+import { Button, Container } from 'semantic-ui-react'
 
-const Hive = ({ hive, handleClick=()=>console.log("No click handler given"), highlighted=[], team=null, interactive=true }) => {
+const Hive = ({ hive, handleClick=()=>console.log("No click handler given"),
+                highlighted=[],
+                team=null,
+                interactive=true,
+                canvasHeight="50vw",
+                cameraOpts={},
+                lookAt=[0, 0, 0]
+},) => {
     const [layout, setLayout] = useState(HEX.layoutFlat)
     let stones = [];
     for (const hex of hive.map.keys()) {
@@ -18,12 +25,13 @@ const Hive = ({ hive, handleClick=()=>console.log("No click handler given"), hig
                 <Stone
                     hex={hex}
                     stone={stone}
+                    key={"" + hex.q + hex.r + stone.insect}
                     height={height}
                     layout={layout}
                     handleClick={handleClick} />
             )
         });
-    }
+    };
     const highlights = [];
     highlighted.forEach(([hex, height]) => {
         highlights.push(<HighlightStone 
@@ -35,16 +43,21 @@ const Hive = ({ hive, handleClick=()=>console.log("No click handler given"), hig
     });
 
     return (
-        <div style={{ position: "relative", height: '50vw' }}>
-            <Canvas camera={{ near: 0.1, far: 100 }} >
+        <Container style={{ height: canvasHeight}}>
+            <Canvas camera={{ near: 0.1, far: 100, ...cameraOpts }}
+                    onCreated={(state) => {
+                        state.camera.lookAt(lookAt[0], lookAt[1], lookAt[2]);
+                    }}
+            >
                 {  interactive ? <TrackballControls /> : null}
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
                 {stones}
                 {highlights}
             </Canvas>
-            <Button onClick={() => setLayout(layout === HEX.layoutFlat ? HEX.layoutPointy : HEX.layoutFlat)}> Toggle Layout </Button>
-        </div>
+            {  interactive ? <Button onClick={() => setLayout(layout === HEX.layoutFlat ? HEX.layoutPointy : HEX.layoutFlat)}> Toggle Layout </Button>: null}
+
+        </Container>
     )
 }
 
