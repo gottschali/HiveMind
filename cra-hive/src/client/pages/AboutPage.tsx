9323468f51b72d8cd82f14ce837861f631c8c287
Gottschali <aligottschall@protonmail.com>
2022-02-16 12:21:07 +0100

    Github link

--- cra-hive/src/client/pages/AboutPage.tsx
+++ cra-hive/src/client/pages/AboutPage.tsx
@@ -1,8 +1,15 @@
 import WelcomeInsect from '../components/WelcomeInsect';
+import { Button, Icon } from 'semantic-ui-react'
+
 
 export default function AboutPage() {
     return <div>
             <WelcomeInsect />
+            <a href='https://github.com/gottschali/HiveMind'>
+                <Button color='grey'>
+                    <Icon name='github' /> Github
+                </Button>
+            </a>
             <iframe src="/README.html" title="Readme" style={{width: "100%", height: "100%", position: "absolute", border: "none"}} />
         </div>
 }
