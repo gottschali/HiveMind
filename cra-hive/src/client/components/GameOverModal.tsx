7245e6536fee9e61c4b77dab395cd8defe64e950
Gottschali <aligottschall@protonmail.com>
2022-02-16 16:49:12 +0100

    More interaction to GameOverModal
    Cancel to go to home or create a new game

--- cra-hive/src/client/components/GameOverModal.tsx
+++ cra-hive/src/client/components/GameOverModal.tsx
@@ -1,29 +1,26 @@
-import { Button, Modal } from 'semantic-ui-react'
+import { Button, Modal, Segment, Header } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { getResult } from '../utils/utils'
+import CreateGameButton from '../components/CreateGameButton'
 
 export default function GameOverModal({ open, result }) {
     const resultText = getResult(result);
     return (
         <Modal
             dimmer='blurring'
-            open={open}
-        >
-            <Modal.Header> Game Over !</Modal.Header>
-            <Modal.Content>
-            {
-                tie ? "It's a tie!" : (whiteWin ? 'Team RED wins' : 'Team BLUE wins')
-            }
-            </Modal.Content>
-            <Modal.Actions>
-                <Button negative as={Link} to='/'> 
-                    Leave
-                </Button>
-            </Modal.Actions>
+            open={open} >
+            <Segment inverted>
                 <Modal.Header>
                     <Header inverted as='h1'>Game over: {resultText} </Header>
                 </Modal.Header>
+                <hr/>
+                <Modal.Actions>
                     <CreateGameButton content="Play another round" />
+                    <Button negative as={Link} to='/'>
+                        Leave
+                    </Button>
+                </Modal.Actions>
+            </Segment>
         </Modal>
     )
 }
