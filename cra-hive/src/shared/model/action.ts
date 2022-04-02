5ff7cb9c39fc483c9eae641489ad6d22f28ada62
Gottschali <aligottschall@protonmail.com>
2022-04-02 19:52:38 +0200

    Use inheritance for action

--- cra-hive/src/shared/model/action.ts
+++ cra-hive/src/shared/model/action.ts
@@ -1,35 +1,38 @@
 import { Hex, hex_compare } from '../hexlib';
 import Stone from './stone';
 
-export class Move {
+export class Action {
+}
+
+export class Move extends Action {
     origin: Hex;
     destination: Hex;
 
     constructor(origin?: Hex, destination?: Hex) {
+        super();
         this.origin = origin;
         this.destination = destination;
     }
 }
 
-export class Drop {
+export class Drop extends Action {
     stone: Stone;
     destination: Hex;
 
     constructor(stone?: Stone, destination?: Hex) {
+        super();
         this.stone = stone;
         this.destination = destination;
     }
 }
 
 export function compareAction(a: Action, b: Action): boolean {
-    if ('origin' in a && 'origin' in b) {
+    if (a instanceof Move && b instanceof Move) {
         return hex_compare(a.origin, b.origin) && hex_compare(a.destination, b.destination)
-    } else if ('stone' in a && 'stone' in b) {
+    } else if (a instanceof Drop && b instanceof Drop) {
         return a.stone.team === b.stone.team && a.stone.insect === b.stone.insect && hex_compare(a.destination, b.destination)
-    } else if (b === Pass) return true;
+    } else if (a instanceof Pass && b instanceof Pass) return true;
     else return false;
 }
 
-export class Pass {};
-
-export type Action = Drop | Move | Pass;
+export class Pass extends Action { };
