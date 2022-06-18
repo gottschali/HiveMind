553b22aee5e8c3387c79f1b6c859e87a9d0b960b
Gottschali <aligottschall@protonmail.com>
2022-06-18 14:09:25 +0200

    Add feedback link

--- cra-hive/src/client/pages/WelcomePage.tsx
+++ cra-hive/src/client/pages/WelcomePage.tsx
@@ -10,15 +10,13 @@ export default function WelcomePage () {
             On this site you can play the awesome game Hive.
             Just create a game with the button in the top-bar and invite a friend to play.
             Make sure check out the <Link to='/tutorial'> tutorial </Link> for an introduction to the rules.
-        Unfortunately the mobile version of this site sucks. If you are on your phone please try it sometime on a bigger screen.
+        Unfortunately the mobile version of this site sucks.
         <h2> The game Hive </h2>
         Hive is a abstract strategic board game designed by John Yianni
-        and published by <Link to='https://www.gen42.com/games/hive'> Gen 42</Link>.
-        If you want to know more about it <Link to='https://boardgamegeek.com/boardgame/2655/hive'>boardgamegeek</Link> is a good place to start.
+        and published by <a href='https://www.gen42.com/games/hive'> Gen 42</a>.
+        If you want to know more about it <a href='https://boardgamegeek.com/boardgame/2655/hive'>boardgamegeek</a> is a good place to start.
 
         <h2> Development </h2>
-        This is a side project of Ali G. In the future you may find more of his projects on <a href='https://gschall.ch/'> gschall.ch </a>.
-
         The frontend uses
         <a href='https://reactjs.org/'> react </a>
         bootstrapped by
@@ -41,5 +39,12 @@ export default function WelcomePage () {
         Just be aware that is my first bigger web-dev project and there are many ugly things lurking in the code.
         {/* <GameList />
         <BackgroundCanvas /> */}
+
+        This is a side project of Ali G. In the future you may find more of his projects on <a href='https://gschall.ch/'> gschall.ch </a>.
+
+        <a href="mailto:post@aligottschall.ch">Send some feedback</a>.
         </div>
+
+
+
 }
